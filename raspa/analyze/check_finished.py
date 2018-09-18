9dc7fee9f1bf899f6c2d47b6a3a4fc49be160e81
kbsezginel <kbs37@pitt.edu>
2018-09-18 10:59:02 -0400

    RASPA check finished checks for not started simulations.

--- raspa/analyze/check_finished.py
+++ raspa/analyze/check_finished.py
@@ -9,22 +9,26 @@ from raspa_output import parse_output
 
 run_dir = sys.argv[1]
 results_file = '%s_results.yaml' % os.path.basename(os.path.abspath(run_dir))
-finished, warnings = 0, 0
+finished, warnings, not_started = 0, 0, 0
 all_results = []
 n_mofs = len(os.listdir(run_dir))
 for i, mof in enumerate(os.listdir(run_dir)):
     print('\rReading results... %3i / %3i' % (i + 1, n_mofs), end='')
     mof_dir = os.path.join(run_dir, mof)
-    ads_path = glob.glob(os.path.join(mof_dir, 'Output', 'System_0', '*.data'))[0]
-    results = parse_output(ads_path, verbose=False, save=False)
-    all_results.append(results)
-    if results['finished'] == True:
-        finished += 1
-    if len(results['warnings']) > 0:
-        warnings += 1
+    ads_path = glob.glob(os.path.join(mof_dir, 'Output', 'System_0', '*.data'))
+    if len(ads_path) > 0:
+        results = parse_output(ads_path, verbose=False, save=False)
+        all_results.append(results)
+        if results['finished'] == True:
+            finished += 1
+        if len(results['warnings']) > 0:
+            warnings += 1
+    else:
+        not_started += 1
 
 print('%i / %i finished' % (finished, n_mofs))
 print('%i / %i warnings' % (warnings, n_mofs))
+print('%i / %i not_started' % (not_started, n_mofs))
 
 with open(results_file, 'w') as resfile:
     yaml.dump(all_results, resfile)
